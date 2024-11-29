74acc601fe88343a850b81aa06f927ce08f9b97e
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2024-11-29 18:55:46 +0500

    Let's learn How to control scroll bar through Scrolltrigger in Episode -3

---
+++ Gsap/3.ScrollTrigger/script.js
@@ -0,0 +1,142 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// gsap.from('#page2 #box',{
+//         scale:0,
+//         duration:2,
+//         rotate:720,
+//         scrollTrigger:{
+//             trigger:"#page2 #box",
+//             scroller:"body",
+//             scrub:true,
+//             scrub:2,
+//             markers:true,
+//             start:"top 60% ",
+//             pin:true,
+//         }
+//     });
+    
+
+
+
+
+
+
+
+
+
+// gsap.from("#page2 h1",{
+//     x:500,
+//     opacity:0,
+//     duration:2,
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scroller:"body",
+//         markers:true,
+//         start:'top 60%'
+//     }
+// });
+
+// gsap.from("#page2 h2",{
+//     x:-500,
+//     opacity:0,
+//     duration:2,
+//     scrollTrigger:{
+//         trigger:"#page2 h2",
+//         scroller:"body",
+//         markers:true,
+//         start:'top 60%'
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// gsap.from('#page1 #box',{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360,
+//     // repeat:1,
+//     // yoyo:true,
+// });
+// gsap.from('#page2 #box',{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60% "
+//     }
+    
+// });
